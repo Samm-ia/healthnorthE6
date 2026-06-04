1bc1cd5d150237dc64cd9dab78111b71fc56bf48
Samia Errabite <samiaerrabite@Air-de-Samia.lan>
2026-06-04 08:46:50 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require_once __DIR__ . '/App/DB/config.php';
 require_once __DIR__ . '/Backend/app/Core/database.php';
 require_once __DIR__ . '/Backend/app/Models/Model.php';
 require_once __DIR__ . '/Backend/app/Models/User.php';
